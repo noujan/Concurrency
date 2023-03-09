20984f787916b8e3aab71e64ad67fef2522c977e
Noujan Fakhri <nfakhri@haystack.ai>
2023-03-09 16:56:55 -0500

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Noujan Fakhri on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
